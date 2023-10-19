257e4c2e3e536c0a9a209694c39a88d04cacfd5d
MilanoVGC <milanovgc@gmail.com>
2023-10-19 16:57:04 +0200

    test

--- sidebar-02/js/main.js
+++ sidebar-02/js/main.js
@@ -17,3 +17,8 @@
   });
 
 })(jQuery);
+
+$("table tr").hide();
+$("table tr").each(function(index){
+	$(this).delay(index*200).show(50);
+});
--- test/script.js
+++
@@ -1,4 +0,0 @@
-$("table tr").hide();
-$("table tr").each(function(index){
-	$(this).delay(index*200).show(50);
-});
